1e5532c070216b1e26c0334769982de7518d24f7
abhijitpadhi <abhijit.padhi@gmail.com>
2017-01-03 17:17:55 +0530

    Login page

---
+++ www/js/controllers.js
@@ -0,0 +1,35 @@
+(function(){
+
+var ctrl = angular.module('controllers', ['ionic']);
+
+
+	ctrl.controller('LoginCtrl', function($scope, $state){
+
+		var user = {
+			customerID: 'admin',
+			password: 'password'
+		};
+		
+		$scope.logIn =function(user){
+			console.log(user);
+			$state.go('main');
+		};
+
+	});
+
+/*
+	$ionicPopup.showAlert = function(){
+		
+		var alertPopup = $ionicPopup.alert({
+			title: 'Login Alert',
+			template: 'Either email ID or password is wrong'
+		});
+
+		alertPopup.then(function(res){
+			if(!res) {
+				console.log('Either email ID or password is wrong')
+			}
+		});
+	};
+*/
+}());
